37a2db90eccb36149e9e9762ec08f6307e247007
R Rakesh Kumar <bngaram14337@gmail.com>
2023-10-17 16:24:03 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BusList from './BusList';
+import ReservationForm from './ReservationForm';
+
+function App() {
+  return (
+    <div>
+      <h1>Bus Reservation System</h1>
+      <BusList />
+      <ReservationForm />
+    </div>
+  );
+}
+
+export default App;
